3339b5d41446a96494489d32404f308511bb1dab
moderateblue <moderatelybluz@gmail.com>
2023-11-03 23:06:34 -0700

    Create medianfunction.cpp

---
+++ medianfunction.cpp
@@ -0,0 +1,27 @@
+#include "datavalidation.h"
+
+using namespace std;
+
+double median(int *arr, int size) {
+    if (size % 2 == 0) {
+        double med = (*(arr + (size/2 - 1)) + *(arr + (size/2))) / 2.0;
+        cout << "if med: " << med << endl;
+        return med;
+    } else {
+        double med = *(arr + size/2);
+        cout << "else med: " << med << endl;
+        return med;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int arr2[] = {1, 2, 3, 4, 5, 7, 8};
+
+    double arrmed = median(&arr[0], 8);
+    double arr2med = median(&arr2[0], 7);
+    
+    cout << arrmed << endl << arr2med << endl;
+
+    return 0;
+}
